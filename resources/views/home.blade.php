42e9d7306961b4d240cc04ec784024f3f107f82c
Shafik Amin <samin@appointmentplus.com>
2018-04-16 18:33:15 -0700

    Added floating message capability.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,10 +14,20 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in.
                 </div>
             </div>
         </div>
+
+        @if (session('message'))
+        <div class="floating-message alert alert-success" role="alert">
+            {{--@php--}}
+            {{--session(['message' => "Test Message!"]);--}}
+            {{--@endphp--}}
+            <b> {{ session('message') }} </b>
+        </div>
+        @endif
+
     </div>
 </div>
 @endsection
