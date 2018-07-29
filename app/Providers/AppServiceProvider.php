17790ee1455fa5caea0ca45e4dd7dc80fe3351d5
Shafik <shafik23@gmail.com>
2018-07-29 00:37:32 -0700

    Forced https everywhere (if prod).

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,22 @@ class AppServiceProvider extends ServiceProvider
         Blade::if('is_super_user', function () {
             return Auth::user()->is_super_user;
         });
+
+        \Schema::defaultStringLength(191);
+
+        // Force "https everywhere"
+        if (config('app.env') != 'local') {
+            \URL::forceScheme('https');
+        }
+
+        // Add a custom "phone" validator field
+        Validator::extend('phone', function($attribute, $value, $parameters, $validator) {
+            return preg_match('%^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$%i', $value) && strlen($value) >= 10;
+        });
+
+        Validator::replacer('phone', function($message, $attribute, $rule, $parameters) {
+            return str_replace(':attribute',$attribute, 'Invalid phone number');
+        });
     }
 
     /**
