6de7af5f99acd4e7ffa4ea35e19b790e579fee65
Shafik <shafik23@gmail.com>
2018-07-31 20:44:07 -0700

    Minor: added better url fetcher.

--- resources/views/echo.blade.php
+++ resources/views/echo.blade.php
@@ -6,7 +6,7 @@
         <strong>Advanced: POST Echo Tester</strong>
         <br>
         This page shows the last several http POSTs that were sent to this endpoint. Try it out: POST some data
-        to <i>{{ Request::url() }}</i>, then hit refresh.
+        to <i>{{ url()->current() }}</i>, then hit refresh.
     </p>
     <hr>
 
