6f799201e74b805ee323efeb0ef2eec2e4fb812e
Shafik <shafik23@gmail.com>
2018-08-25 03:12:54 -0700

    Added GoogleAnalytics.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,17 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ env('GOOGLE_ANALYTICS_TRACKING_ID') }}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', {{ env('GOOGLE_ANALYTICS_TRACKING_ID') }});
+    </script>
+
 </head>
 <body>
     <div id="app">
