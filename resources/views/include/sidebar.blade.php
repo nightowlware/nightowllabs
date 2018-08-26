13fbf1d722f537118d203425edfac149ad35bd5f
Shafik <shafik23@gmail.com>
2018-08-26 02:31:13 -0700

    Fixed minor issue with detecting Request::url.

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -3,7 +3,7 @@
 <div class="card sidebar col-sm-2 col-md-2 col-lg-2 pull-left">
     <div style="padding-bottom: 50px;">
         @foreach ($usermenu as $name => $details)
-            @if (Request::url() == $details['url'])
+            @if (Request::url() == str_replace('https', 'http', $details['url']))
                 <a class="flex-center sidebar-button btn btn-warning text-secondary btn-lg" href="{{$details['url']}}" role="button">
                     <div style="padding: 5px;">
                         <i class="fa {{$details['icon']}}"></i>
