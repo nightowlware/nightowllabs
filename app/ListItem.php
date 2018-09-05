deff4335eb8235d0ca3d1eaf23e60787666abcbf
Shafik <shafik23@gmail.com>
2018-09-04 23:30:55 -0700

    Added query scope to ListItems

--- app/ListItem.php
+++ app/ListItem.php
@@ -3,10 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListItem extends Model
 {
     public function checklist() {
         return $this->belongsTo('App\Checklist');
+    }
+
+    /**
+     * The "boot" method of this model
+     */
+    protected static function boot() {
+        parent::boot();
+
+        static::addGlobalScope('checklist', function (Builder $builder) {
+            $builder
+                ->join('checklists', 'list_items.checklist_id', '=', 'checklists.id')
+                ->where('user_id', '=', \Auth::id());
+        });
     }
 }
