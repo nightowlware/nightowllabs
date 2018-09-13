8e3800e15fc21fdc8ecdbbf9ec3b0aaeeec2057f
Shafik <shafik23@gmail.com>
2018-09-12 23:49:33 -0700

    Added sort_order cols.

---
+++ database/migrations/2018_09_12_190525_amend_checklists_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AmendChecklistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach (['checklists', 'list_items'] as $t) {
+            Schema::table($t, function (Blueprint $table) {
+                $table->integer('sort_order');
+            });
+
+            // set the order to the id by default
+            DB::update('update '.$t.' set sort_order = id');
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        foreach (['checklists', 'list_items'] as $t) {
+            Schema::table($t, function (Blueprint $table) {
+                $table->dropColumn('sort_order');
+            });
+        }
+    }
+}
