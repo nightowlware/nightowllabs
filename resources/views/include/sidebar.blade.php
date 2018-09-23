529872bc390267cba233bee3c38186811605bb62
Shafik <shafik23@gmail.com>
2018-09-23 13:30:34 -0700

    Added sidebar collapse toggle.

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -1,6 +1,6 @@
 @inject('usermenu', 'usermenu')
 
-<div class="card sidebar col-3 col-sm-2 col-md-2 col-lg-2 pull-left">
+<div id="sidebar" class="card sidebar col-3 col-sm-2 col-md-2 col-lg-2 pull-left">
     <div style="padding-bottom: 50px;">
         @foreach ($usermenu as $name => $details)
             @if (Request::url() == str_replace('https', 'http', $details['url']))
@@ -24,5 +24,30 @@
             @endif
         @endforeach
     </div>
+
+    <button id="sidebar-toggle" type="button" class="btn btn-light" onclick="toggleSideBar()">
+        <i class="fas fa-angle-double-left"></i>
+    </button>
+    <script>
+        function toggleSideBar() {
+            $('#sidebar').toggleClass("col-3 col-sm-2 col-md-2 col-lg-2");
+            $('#sidebar').toggleClass("col-2 col-sm-1 col-md-1 col-lg-1 p-1 m-1 thin-border");
+
+            if ($('#sidebar').hasClass('col-3')) {
+                $('#sidebar-toggle').html('<i class="fas fa-angle-double-left"></i>');
+            } else {
+                $('#sidebar-toggle').html('<i class="fas fa-angle-double-right"></i>');
+            }
+        }
+    </script>
+    <style>
+        #sidebar-toggle {
+            margin-bottom: 30px;
+        }
+
+        .thin-border {
+            border-width: 3px !important;
+        }
+    </style>
 </div>
 
