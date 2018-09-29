cff0bbded6bbbc53c403ec4a70039308371a09af
Shafik <shafik23@gmail.com>
2018-09-29 15:25:10 -0700

    Still broken.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,9 @@
 @section('content')
 <div class="container">
     <dashboard></dashboard>
+
+    <passport-clients></passport-clients>
+    <passport-authorized-clients></passport-authorized-clients>
+    <passport-personal-access-tokens></passport-personal-access-tokens>
 </div>
 @endsection
