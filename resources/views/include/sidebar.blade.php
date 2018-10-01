437c5328f576a6834cca6fce5343f3cb4fc3e028
Shafik <shafik23@gmail.com>
2018-10-01 08:58:15 -0700

    Minor - added sidebar highlighting for https.

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -3,7 +3,7 @@
 <div id="sidebar" class="card sidebar col-3 col-sm-2 col-md-2 col-lg-2 pull-left">
     <div style="padding-bottom: 50px;">
         @foreach ($usermenu as $name => $details)
-            @if (Request::url() == str_replace('https', 'http', $details['url']))
+            @if (Request::url() == str_replace('https', 'http', $details['url']) || Request::url() == $details['url'])
                 <a class="flex-center sidebar-button btn btn-warning text-secondary btn-lg" href="{{$details['url']}}" role="button">
                     <div style="padding: 5px;">
                         <i class="{{$details['icon']}}"></i>
