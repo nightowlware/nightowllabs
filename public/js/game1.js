5b09e28096c0838b7028e23f738a0917847dc814
Shafik <shafik23@gmail.com>
2018-12-24 04:22:49 -0700

    Added simple cursor key bindings to phaser game.

--- public/js/game1.js
+++ public/js/game1.js
@@ -18,10 +18,28 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-// On every frame update
+// On every frame update.
 function update() {
-    this.xVelocity = (this.xVelocity + (Math.random() * 50 - 25)) % 200;
-    this.yVelocity = (this.yVelocity + (Math.random() * 50 - 25)) % 200;
+    const delta = 20;
+    const cap = 200;
+    this.xVelocity = (this.xVelocity + (Math.random() * delta - delta/2)) % cap;
+    this.yVelocity = (this.yVelocity + (Math.random() * delta - delta/2)) % cap;
+
+    if (this.cursorKeys.left.isDown) {
+        this.xVelocity -= 50;
+    }
+
+    if (this.cursorKeys.right.isDown) {
+        this.xVelocity += 50;
+    }
+
+    if (this.cursorKeys.up.isDown) {
+        this.yVelocity -= 50;
+    }
+
+    if (this.cursorKeys.down.isDown) {
+        this.yVelocity += 50;
+    }
 
     this.logo.setVelocity(this.xVelocity, this.yVelocity);
 }
@@ -32,7 +50,16 @@ function preload() {
     this.load.image('sky', 'sky.jpg');
 }
 
+// Sets up the initial game state.
 function create() {
+    this.input.keyboard.on('keydown_A', () => {
+        this.xVelocity -= 40;
+        console.log(this.xVelocity);
+    });
+
+    // for some reason, cursor (arrow keys) don't support callbacks like above.
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
+
     this.add.image(400, 300, 'sky');
 
     let particles = this.add.particles('logo');
