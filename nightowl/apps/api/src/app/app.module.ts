1e2b1328d41878063639fb995d8e1e9924b4db55
Shafik <shafik23@gmail.com>
2019-11-20 01:20:23 -0700

    Got TypeORM/MongoDB hooked up - still need entities/repositories/etc.

--- nightowl/apps/api/src/app/app.module.ts
+++ nightowl/apps/api/src/app/app.module.ts
@@ -1,17 +1,33 @@
-import { CacheModule, Module, HttpModule } from '@nestjs/common';
+import { Logger, CacheModule, Module, HttpModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { environment } from '../environments/environment';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Connection } from 'typeorm';
 
 @Module({
   imports: [
     HttpModule,
-    CacheModule.register({
-      ttl: 5,
-      max: 100
+    CacheModule.register({ ttl: 5, max: 100 }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: process.env.MONGO_HOST || 'localhost',
+      port: Number(process.env.MONGO_PORT) || 27017,
+      username: process.env.MONGO_USERNAME || 'nootz',
+      password: process.env.MONGO_PASSWORD || 'nootz',
+      database: 'nootz',
+      entities: [],
+      synchronize: !environment.production,
+      useUnifiedTopology: true
     })
   ],
   controllers: [AppController],
   providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {
+    const { password, ...others } = connection.options as any;
+    Logger.log(`DB connection info: ${JSON.stringify(others)}`);
+  }
+}
--- nightowl/apps/api/src/main.ts
+++ nightowl/apps/api/src/main.ts
@@ -1,12 +1,11 @@
-/**
- **/
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import * as dotenv from 'dotenv';
 
 // Enable configuration-by-env file: key-value pairs
 // are injected into the "process.env" object.
+// Note that a reboot is required every time the .env values
+// change.
 dotenv.config();
 
 async function bootstrap() {
@@ -16,7 +15,6 @@ async function bootstrap() {
 
   // Should read from local .env file
   const port = process.env.API_PORT || 6666;
-  console.log(process.cwd());
 
   await app.listen(port, () => {
     console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
