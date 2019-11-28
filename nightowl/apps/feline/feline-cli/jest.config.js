a55fa40d281f7ebf9ff5f51edb4d595ea7feebb4
Sidewinder1138 <andersonlambda@gmail.com>
2019-11-28 11:26:46 -0700

    Starting on a TS version of Feline :)

---
+++ nightowl/apps/feline/feline-cli/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'feline-feline-cli',
+  preset: '../../../jest.config.js',
+  coverageDirectory: '../../../coverage/apps/feline/feline-cli'
+};
---
+++ nightowl/apps/feline/feline-cli/src/main.ts
@@ -0,0 +1,2 @@
+
+console.log('Hello there, I am the Feline CLI!');
