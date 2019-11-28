953bc3e19d871d1fe43dd8dbb4c4576d329eb73d
Sidewinder1138 <andersonlambda@gmail.com>
2019-11-28 12:00:58 -0700

    feline: more on repl

--- nightowl/apps/feline/feline-cli/src/app/feline.ts
+++ nightowl/apps/feline/feline-cli/src/app/feline.ts
@@ -4,18 +4,18 @@ import * as repl from 'repl';
 export class Feline {
 
     runRepl() {
-        console.log("TODO: run the repl!");
-
-        const evaluator = (cmd, ctx, filename, cb) => {
-            console.log('cmd =', cmd);
-            console.log('filename = ', filename);
-            console.log('message =', ctx.msg);
-            cb(null, 'Test!');
+        const evaluator = (cmd: string, ctx: any, filename: string, cb: any) => {
+            const value = this.run(cmd);
+            cb(null, value); //TODO: read up on how this callback works...
         }
 
-        const msg = 'hey there';
-        repl.start({ prompt: '> ', eval: evaluator}).context.msg = msg;
+        const msg = 'some data for the context';
 
+        repl.start({ prompt: '> ', eval: evaluator}).context.msg = msg;
+    }
+
+    run(code: string) : any {
+        return "TODO: Run the code!";
     }
 
 }
--- nightowl/apps/feline/feline-cli/src/main.ts
+++ nightowl/apps/feline/feline-cli/src/main.ts
@@ -1,7 +1,7 @@
 import { Feline } from "./app/feline";
 
 function main() {
-    console.log('Feline CLI v0.0.2');
+    console.log('\nFeline CLI v0.0.2');
 
     const f = new Feline();
     f.runRepl();
